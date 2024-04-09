24a1d754965136f8c3c9daf6f7582cd24e0613c0
wjdals Lee <kyungheeyj45@ajou.ac.kr>
2024-04-10 08:42:04 +0900

    [백준] - 2839

---
+++ Columbus Study 2기/4주차/0410/2839.py
@@ -0,0 +1,15 @@
+N = int(input())
+dp = [-1]*5001
+dp[3] = 1
+dp[5] = 1
+
+
+for i in range(6, N+1):
+    if dp[i-5] != -1:
+        dp[i] = dp[i-5] + 1
+        continue
+
+    if dp[i-3] != -1:
+        dp[i] = dp[i-3] + 1
+
+print(dp[N])
