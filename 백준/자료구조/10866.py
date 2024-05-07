1a074c17b440a6db29e8eeefda3ff6a179bb9d60
wjdals Lee <kyungheeyj45@ajou.ac.kr>
2024-05-07 17:42:47 +0900

    [백준] - 10866

---
+++ 백준/자료구조/10866.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+N = int(sys.stdin.readline())
+data = deque([])
+
+for _ in range(N):
+    cmd = sys.stdin.readline().split()
+    if cmd[0] == "push_front":
+        data.appendleft(cmd[1])
+    if cmd[0] == "push_back":
+        data.append(cmd[1])
+    elif cmd[0] == "pop_front":
+        if len(data) == 0:
+            print(-1)
+        else:
+            print(data.popleft())
+    elif cmd[0] == "pop_back":
+        if len(data) == 0:
+            print(-1)
+        else:
+            print(data.pop())
+    elif cmd[0] == "size":
+        print(len(data))
+    elif cmd[0] == "empty":
+        if len(data) == 0:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == "front":
+        if len(data) == 0:
+            print(-1)
+        else:
+            print(data[0])
+    elif cmd[0] == "back":
+        if len(data) == 0:
+            print(-1)
+        else:
+            print(data[-1])
