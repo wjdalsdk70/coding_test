47304d1f9fa19c736268158a02f49d36df6eae62
이정민 <kyungheeyj45@ajou.ac.kr>
2024-05-21 15:01:53 +0900

    [백준] - 10818(java)

---
+++ 백준/Java/bj10818.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class bj10818 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0]+" "+arr[N-1]);
+    }
+}
